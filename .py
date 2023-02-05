22ab60e4c52f0049a8a2b2345b7c943d56590ba3
Chirag Tankan <118548447+Tankanchirag@users.noreply.github.com>
2023-02-05 13:36:05 +0530

    Update and rename .gitignore to .py

---
+++ .py
@@ -0,0 +1,16 @@
+import numpy as np
+
+intarr = np.array([[1, 2, 3], [4, 5, 6]])
+print("Integer array:")
+print(intarr)
+print("")
+
+floatarr = np.array([[1., 2, 3], [4, 5, 6]])
+print("Float array:")
+print(floatarr)
+print("")
+
+complexarr = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.complex_)
+print("Complex array:")
+print(complexarr)
+print("")
